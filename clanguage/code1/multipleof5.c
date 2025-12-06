34dadaed85f1d606936ec601cb378ce25c68b20d
Sanjay Singh Gurjar <sanjaysinghgurjar_25rco02@dtu.ac.in>
2025-12-06 11:09:28 +0530

    update

---
+++ clanguage/code1/multipleof5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int multi;
+    scanf("%d",&multi);
+    if(multi%5==0)printf("multiple of 5");
+    else
+    printf("not a multiple");
+    return 0;
+}
+    
