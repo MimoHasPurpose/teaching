d85ab55dd8a8e728c829413db0b984a5e76cae6a
Sanjay Singh Gurjar <sanjaysinghgurjar_25rco02@dtu.ac.in>
2025-12-07 22:53:56 +0530

    update

--- clanguage/code1/strings.c
+++ clanguage/code1/strings.c
@@ -2,12 +2,28 @@
 // 1. access values in strings
 // 2. modify values in strings
 // 3. loop through a string
-//  0  1  2 3 4 5 6
+
+
+// notes:
+//  char value : 'a' ;
+
+//  string values: "abac";
+
 #include<stdio.h>
 int main(){
    
-    char name[]="Sanjay";
+    char name[]="111111";
     printf("%s\n",name);
+    // to access a value we use %c and use array indexing to get the specific value.
     printf("%c\n",name[2]);
+    //  modifying string values
+    name[2]='N';
+    printf("%c\n",name[2]);
+    //  loop through a string
+    for(int i=0;i<=5;i++){
+        printf("%c\n",name[i]);
+
+    }
+
     return 0;
 }
---
+++ clanguage/functions.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+
+
+
+}
+
+int multiplication(int a, int b, int c, int d, int e){
+
+}
+
+void print10(string name){
+
+
+}
+
+void motivation(){
+
+
+}
