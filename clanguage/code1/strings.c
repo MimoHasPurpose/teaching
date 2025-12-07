1af0ad48ba639f915da51bee2e4f638397f8d63c
Sanjay Singh Gurjar <sanjaysinghgurjar_25rco02@dtu.ac.in>
2025-12-07 22:36:51 +0530

    update

---
+++ clanguage/code1/strings.c
@@ -0,0 +1,13 @@
+// task:
+// 1. access values in strings
+// 2. modify values in strings
+// 3. loop through a string
+//  0  1  2 3 4 5 6
+#include<stdio.h>
+int main(){
+   
+    char name[]="Sanjay";
+    printf("%s\n",name);
+    printf("%c\n",name[2]);
+    return 0;
+}
---
+++ clanguage/readme.md
@@ -0,0 +1,10 @@
+## c language 
+1. while loop
+2. variables
+3. loops
+4. functions
+5. structures
+6. searching
+7. recursion
+8. strings
+9. geeksforgeeks questions
