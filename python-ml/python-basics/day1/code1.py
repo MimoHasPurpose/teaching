956c4eafde9b2b54c88dda260b620a991c7b7b18
MimoHasPurpose <sanjaysinghgurjar_25rco02@dtu.ac.in>
2025-12-20 17:38:28 +0530

    update

---
+++ python-ml/python-basics/day1/code1.py
@@ -0,0 +1,7 @@
+#  basics
+a=10
+b=20
+a="Sanjay"
+print(a)
+print("hello world !!")
+
---
+++ python-ml/python-basics/day1/turtle.py
@@ -0,0 +1,12 @@
+from turtle import *
+pensize(4) 
+forward(200)
+left(90)  
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+left(90)
+
+done()  
