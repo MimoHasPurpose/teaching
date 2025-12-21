ad2d28a251c91835bcb0a21a5339e930b75600f7
MimoHasPurpose <sanjaysinghgurjar_25rco02@dtu.ac.in>
2025-12-21 06:59:00 +0530

    up

---
+++ python-ml/python-basics/day2/notes.md
@@ -0,0 +1,6 @@
+common errors:
+1. ModuleNotFoundError
+2. NameError
+3. TypeError
+4. IndentationError
+5. SyntaxError
---
+++ python-ml/python-basics/day2/turt.py
@@ -0,0 +1,10 @@
+from turtle import *
+
+pensize(4)
+speed('fastest')
+# goto(400,400)
+for i in range(100):  # Loop 50 times instead of 4.
+    forward(50)
+    left(61)
+# hideturtle()
+done()
